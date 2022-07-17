d749a6e8f70fe85f0ff7a00f200e0a9a6927da94
Malik <permalik@protonmail.com>
2022-07-17 09:46:16 -0500

    Update styles for abbreviation

--- src/components/app/card/Card.tsx
+++ src/components/app/card/Card.tsx
@@ -12,24 +12,36 @@ const Card = ({abbreviation, name, toggle}: Props) => {
       className="
       cursor-pointer
       min-w-sm
-      px-3
-      py-2
+      px-5
+      py-4
       rounded-md
       shadow-current
       shadow-sm"
       onClick={toggle}
     >
-      <div>
-        <section>
-          <div>
+      <div className="
+      flex
+      flex-row
+      justify-between">
+        <div>
+          <div className="
+          bg-slate-600
+          flex
+          flex-row
+          items-center
+          justify-center
+          rounded-full"
+               style={{height: "50px", width: "50px"}}>
             <p className="text-slate-300">
               {abbreviation}
             </p>
           </div>
-        </section>
-        <h2 className="text-slate-300">
-          {name}
-        </h2>
+        </div>
+        <div>
+          <h2 className="text-slate-300">
+            {name}
+          </h2>
+        </div>
       </div>
     </li>
   );
